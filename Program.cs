e668f44c71aeaeb4d0f7a77c8cefb4ac9830e01d
Tommy Waalkes <Tommy@grandcircus.co>
2019-07-08 16:15:38 -0400

    Added Stuff

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long total = Factorial(1, 1000, 1);
+
+            Console.WriteLine("The factorial of 1000 is " + total);
+        }
+
+        public static long Factorial(long currentTotal, int endIndex, int currentIndex)
+        {
+            Console.WriteLine("Current Index: "+ currentIndex);
+            Console.WriteLine("Current Total: " + currentTotal);
+            if(currentTotal == 0)
+            {
+                return currentTotal;
+            }
+            Console.WriteLine();
+            if(currentIndex> endIndex)
+            {
+                return currentTotal;
+            }
+            else
+            {
+                currentTotal *= currentIndex;
+
+                currentIndex++;
+
+                return Factorial(currentTotal, endIndex, currentIndex);
+            }
+        }
+    }
+}
